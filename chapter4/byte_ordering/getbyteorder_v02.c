e7aee99c44ab42070823cf9bdfd31d01dea1db54
skinnm13 <mjcs2002@gmail.com>
2026-02-18 19:44:48 -0500

    added the to_int implementation

--- chapter4/byte_ordering/getbyteorder_v02.c
+++ chapter4/byte_ordering/getbyteorder_v02.c
@@ -31,6 +31,20 @@ long int from_bytes(uint32_t bytes, char endian) {
   return converted;
 }
 
+uint32_t to_int(long int num, char endian){
+  uint32_t bytes = 0;
+  uint8_t b1 = (num >> 24) & 0xFF;
+  uint8_t b2 = (num >> 16) & 0xFF;
+  uint8_t b3 = (num >> 8)  & 0xFF;
+  uint8_t b4 = num & 0xFF;
+  if(endian == 'b') {
+    bytes = (b1 << 24) + (b2 << 16) + (b3 << 8) + b4;
+  }
+  else if(endian == 'l') {
+    bytes = (b4 << 24) + (b3 << 16) + (b2 << 8) + b1;
+  }
+  return bytes;
+}
 
 int main(void)
 { 
@@ -40,10 +54,13 @@ int main(void)
 
   printf("Integer: %x %x %x %x\n", c[0], c[1], c[2], c[3]); 
   printf("String:  %x %x %x %x\n", d[0], d[1], d[2], d[3]);
-  char endian;
-  scanf("%c", &endian);
+  char endian, endian2;
+  scanf("%c\r\n", &endian);
   long int converted = from_bytes(a, endian);
   printf("Converted: %ld\n", converted);
+  scanf("%c\r\n", &endian2);
+  uint32_t bytes = to_int(converted, endian2);
+  printf("Bytes: %x\n", bytes);
 
   return 0;
 }
