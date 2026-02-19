71386a80172d9ea9e10efc70f44288a2e970c044
skinnm13 <mjcs2002@gmail.com>
2026-02-18 19:38:55 -0500

    Made the implementation more accurate

--- chapter4/byte_ordering/getbyteorder_v02.c
+++ chapter4/byte_ordering/getbyteorder_v02.c
@@ -3,8 +3,24 @@
 #include <stdint.h> 
 
 
-long int convert2dec(uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, char endian) {
+// long int convert2dec(uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, char endian) {
+//   long int converted = 0;
+
+//   if(endian == 'b') {
+//     converted += b1*256*256*256 + b2*256*256 + b3*256 + b4;
+//   }
+//   else if(endian == 'l') {
+//     converted += b4*256*256*256 + b3*256*256 + b2*256 + b1;
+//   }
+//   return converted;
+// }
+
+long int from_bytes(uint32_t bytes, char endian) {
   long int converted = 0;
+  uint8_t b1 = (bytes >> 24) & 0xFF;  // 0x01
+  uint8_t b2 = (bytes >> 16) & 0xFF;  // 0x02
+  uint8_t b3 = (bytes >> 8)  & 0xFF;  // 0x03
+  uint8_t b4 = bytes & 0xFF;          // 0x04
 
   if(endian == 'b') {
     converted += b1*256*256*256 + b2*256*256 + b3*256 + b4;
@@ -25,12 +41,8 @@ int main(void)
   printf("Integer: %x %x %x %x\n", c[0], c[1], c[2], c[3]); 
   printf("String:  %x %x %x %x\n", d[0], d[1], d[2], d[3]);
   char endian;
-  uint8_t b1 = (a >> 24) & 0xFF;  // 0x01
-  uint8_t b2 = (a >> 16) & 0xFF;  // 0x02
-  uint8_t b3 = (a >> 8)  & 0xFF;  // 0x03
-  uint8_t b4 = a & 0xFF;          // 0x04
   scanf("%c", &endian);
-  long int converted = convert2dec(b1, b2, b3, b4, endian);
+  long int converted = from_bytes(a, endian);
   printf("Converted: %ld\n", converted);
 
   return 0;
